88db7b28c8c1d404e52e41240258f4fa5f82518c
EfranaOD <efrana508@gmail.com>
2025-08-08 12:12:40 +0530

    Add arithmetic operator tasks and notes

---
+++ 01-basics/operators/arithmetic-operator/modulus-usage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modulus_usage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user for a number, check if it’s even or odd using %.
+            int userInput;
+            Console.Write("enter any number: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            if(userInput % 2 == 0)
+            {
+                Console.WriteLine($"{userInput} is even");
+            }
+            else
+            {
+                Console.WriteLine($"{userInput} is odd");
+            }
+        }
+    }
+}
