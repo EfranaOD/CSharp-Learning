05997b97deb1e25501d2028db0dae98aa38a3c73
EfranaOD <efrana508@gmail.com>
2025-08-09 23:28:51 +0530

    Remove incorrect nested operators/arithmetic-operators folder

--- 01-basics/variables/var-assignments/05-scope-variable/Program.cs
+++ 01-basics/variables/var-assignments/05-scope-variable/Program.cs
@@ -12,12 +12,15 @@ namespace _05_scope_variable
         {
             int x = 5, y, z;
             {
+                x = 10;
                 y = x + 10;
                 x = x + y;
                 z = x + y;
                 Console.WriteLine($"X: {x}, Y: {y}, Z: {z}"); //x=20(5+10+5), y=15(5+10), z=35(20+15)
             }
             Console.WriteLine($"X: {x}, Y: {y}, Z: {z}");
+
+
         }
     }
 }
