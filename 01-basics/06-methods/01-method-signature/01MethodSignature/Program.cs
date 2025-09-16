95866cbde3c7711c7c6496a06cabaa219e42cd4f
EfranaOD <efrana508@gmail.com>
2025-09-16 18:27:12 +0530

    Add Methods topic: Method Signature

---
+++ 01-basics/06-methods/01-method-signature/01MethodSignature/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01MethodSignature
+{
+    internal class Program
+    {
+        public static string FullName(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+        static void Main(string[] args)
+        {
+            //Takes first name and last name as parameters.
+            //Returns the full name as a single string.
+            //Call this method from Main() and print the result.
+
+            string result = FullName("Efrana", "Mohammed");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01-basics/06-methods/01-method-signature/02CalculateAverage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02CalculateAverage
+{
+    internal class Program
+    {
+        public static double CalculateAverage(int english, int maths, int science)
+        {
+            double average = (english + maths + science) / 3.0;
+            return average;
+        }
+        static void Main(string[] args)
+        {
+            //Create a method that takes three integers (marks in 3 subjects), calculates the average, and returns it as a double.
+            //In Main, call the method with sample values, store the result, and print:
+            //the average and whether the student is Pass(average ≥ 40) or Fail.
+
+            Console.WriteLine("Enter marks in three subjects.");
+            Console.Write("English: ");
+            bool englishMarksSucc = int.TryParse(Console.ReadLine(), out int englishMarks);
+            if (!englishMarksSucc) { Console.WriteLine("Invalid input"); return; }
+
+            Console.Write("Maths: ");
+            bool mathsMarksSucc = int.TryParse(Console.ReadLine(), out int mathsMarks);
+            if (!mathsMarksSucc) { Console.WriteLine("Invalid input"); return; }
+
+            Console.Write("Science: ");
+            bool scienceMarksSucc = int.TryParse(Console.ReadLine(), out int scienceMarks);
+            if (!scienceMarksSucc) { Console.WriteLine("Invalid input"); return; }
+
+            double average = CalculateAverage(englishMarks, mathsMarks, scienceMarks);
+            string result = average >= 40 ? "Student pass" : "Student fail";
+            Console.WriteLine($"Average marks: {average:F2} -> {result}");
+        }
+    }
+}
