cbfe16157a35491bc6d07cc242042c55f3f8da99
EfranaOD <efrana508@gmail.com>
2025-09-16 20:43:47 +0530

    Add Methods topic: Parameters - Value Parameters

---
+++ 01-basics/06-methods/02-parameters/01-value-parameters/01Square/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01Square
+{
+    internal class Program
+    {
+        public static void Square(int number)
+        {
+            number = number*number;
+            Console.WriteLine($"Value inside method: {number}");
+        }
+        static void Main(string[] args)
+        {
+            int number = 50;
+            Square(number);
+
+            Console.WriteLine($"Value outside method: {number}");
+        }
+    }
+}
---
+++ 01-basics/06-methods/02-parameters/01-value-parameters/02Cube/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02Cube
+{
+    internal class Program
+    {
+        public static void Cube(int number)
+        {
+            number = number * number * number;
+            Console.WriteLine($"Cube value inside method: {number}");
+        }
+        static void Main(string[] args)
+        {
+            //Write a method that takes an integer as a value parameter, calculates its cube (number * number * number), and prints the cube inside the method.
+
+            int number = 5;
+            Cube(number);
+
+            Console.WriteLine($"Original value outside method: {number}");
+        }
+    }
+}
