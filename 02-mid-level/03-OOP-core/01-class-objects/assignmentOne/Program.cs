415202482363b40e2e4ec3fc4003f8ae6747b8fa
EfranaOD <efrana508@gmail.com>
2026-01-26 18:56:19 +0530

    Add classes and objects topic with practice and assignment to OOP core

---
+++ 02-mid-level/03-OOP-core/01-class-objects/assignmentOne/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace assignmentOne
+{
+    class Person
+    {
+        public string ID { get; }
+        private static int _personCount;
+        public string _name { get; private set; }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Age cannot be negative");
+                _age = value;
+            }
+        }
+        public Person(string name, int Age, string iD)
+        {
+            _name = name;
+            this.Age = Age;
+            ID = iD;
+            _personCount++;
+        }
+        public static int PersonCount
+        {
+            get { return _personCount; }
+        }
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {this._name} and I am {this.Age} years old.");
+        }
+        public void ChangeName(string newName)
+        {
+            if (newName == null)
+                throw new Exception("Name cannot be null");
+            _name = newName;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person("Dazai Osamu", 39, "P1001");
+            personOne.Introduce();
+
+            Person personTwo = new Person("Nakahara Chuuya", 30, "P1002");
+            personTwo.Introduce();
+
+            Person personThree = new Person("Akutagawa Ryunosuke", 35, "P1003");
+            personThree.Introduce();
+
+            //personOne._name = "efrana";
+
+            Console.WriteLine($"Total Person created: {Person.PersonCount}");
+        }
+    }
+}
---
+++ 02-mid-level/03-OOP-core/01-class-objects/practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace practice
+{
+    class Car
+    {
+        private string Brand;
+        private string Model;
+        private int Mileage;
+        public Car(string brand, string model, int mileage)
+        {
+            Brand = brand;
+            Model = model;
+            Mileage = mileage;
+        }
+        public void StartEngine()
+        {
+            Console.WriteLine("Engine started");
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Driving!");
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Brand: {this.Brand}, Model: {this.Model}, Mileage: {this.Mileage}.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Mercedes", "xxx", 25);
+            car.StartEngine();
+            car.Drive();
+            car.ShowDetails();
+        }
+    }
+}
